486ed21f7536fe21c632637c9b9d3e972e464e66
bartek <bartlomiejszczypior@gmail.com>
2020-05-20 20:38:03 +0200

    Updates

---
+++ precache-manifest.02d98dc1dc77a2c38355ebe5dbe255ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef9b81bc9c9b3912a9e4b4bb093de4fd",
+    "url": "/to-buy/index.html"
+  },
+  {
+    "revision": "b4015e60c6de0c8427e5",
+    "url": "/to-buy/static/css/main.86423d3f.chunk.css"
+  },
+  {
+    "revision": "1b6bb7382771139a3649",
+    "url": "/to-buy/static/js/2.e3369de8.chunk.js"
+  },
+  {
+    "revision": "07a707eba59e3e3ed782f77b31f34780",
+    "url": "/to-buy/static/js/2.e3369de8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4015e60c6de0c8427e5",
+    "url": "/to-buy/static/js/main.1e74b777.chunk.js"
+  },
+  {
+    "revision": "96e398d44123b2fbc5a8",
+    "url": "/to-buy/static/js/runtime-main.5afb06e2.js"
+  }
+]);
